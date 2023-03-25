37f4e2d165f9ee09c5a33f087e9a7e3d4db12627
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2023-03-25 15:48:28 +0100

    recipe API update

---
+++ app/recipe/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+# router.register('recipes', views.RecipeForAll)
+app_name = 'recipe'
+urlpatterns = [path('', include(router.urls))]
