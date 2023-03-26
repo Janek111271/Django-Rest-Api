acfe58e063e714a040ff78e9a9e0c72c78c06847
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2023-03-26 21:31:41 +0200

    add filtering

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Repozytorium stworzone na potrzeby zadania rekrutacyjnego do firmy BitByBit
 Aplikację należy włączyć poprzez wydanie komendy: "docker-compose run" i wejść na adres "localhost:8000/api/docs/"
 
 Aplikacja jest backendem do zarządzania przepisami. Można aplikacji dodawać, usuwać i edytować składniki, przepisy i
-etykiety. Dodatkowo endpoint do pobierania listy przepisów jest publiczny.
+etykiety. Dodatkowo endpoint do pobierania listy przepisów jest publiczny oraz ma możliwość filtrowania po składnikach
+i etykietach.
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,20 +2,50 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, \
     IsAuthenticated
+from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter, OpenApiTypes
 
 from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of tag IDs to filter',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            ),
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_queryset(self):
+    def _params_to_ints(self, qs):
 
-        return self.queryset.order_by('-id')
+        return [int(str_id) for str_id in qs.split(',')]
+
+    def get_queryset(self):
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.order_by('-id').distinct()
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -27,16 +57,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            ),
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.ListModelMixin,
                             viewsets.GenericViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
 
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
